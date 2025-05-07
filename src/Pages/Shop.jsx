1f31d818be48ce91896a65c120d5d10cd9e3adec
Abhishek Dhiman <dhimanabhishek2001@gmail.com>
2025-05-07 17:15:58 +0530

    Complete Home Page, NavBar and Footer

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import Newcollection from '../Components/NewCollections/Newcollection'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const Shop = () => {
   return (
     <>
     <Hero />
     <Popular />  
+    <Offers />
+    <Newcollection />
+    <NewsLetter />
     </>
   )
 }

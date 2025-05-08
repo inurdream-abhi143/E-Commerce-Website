b58d3a97b84cf7c07b5ef481f3ada842d52d3a6f
Abhishek Dhiman <dhimanabhishek2001@gmail.com>
2025-05-08 15:22:42 +0530

    Add login page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import "./CSS/LoginSignup.css"
 
 const LoginSignup = () => {
   return (
     <div>
-      <form className='login-form'>
+      <form className='loginsignup'>
+        <div className="loginsignup-container">
+          <h1>Sign Up</h1>
+          <div className="loginsignup-fields">
+            <input type='text' placeholder='Your Name' />
+            <input type='email' placeholder='Enter your email'/>
+            <input type='password' placeholder='Enter Password' />
+          </div>
+          <button>Continue</button>
+          <p className='loginsignup-login' >Already have an Account <span>Login here</span></p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing i agree to the term of use & privacy ploicy.</p>
+          </div>
+        </div>
         
          
       </form>
